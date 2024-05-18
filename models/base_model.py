f76ce62be3be02d6da2b0b9c57180db8433b0788
aloyaaltuff <Aloyaaltuff@gmail.com>
2024-05-18 11:33:42 -0700

    all

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,56 @@
 #!/usr/bin/python3
-"""This script is the base model"""
-
+import json
 import uuid
 from datetime import datetime
-from models import storage
+"""the class that all classes will inherit from"""
 
 
-class BaseModel:
-
-    """Class that  all other classes will inherit from"""
+class BaseModle:
 
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+                        else:
+                            self.id = str(uuid.uuid4())
+                            self.created_at = datetime.now()
+                            self.updated_at = datetime.now()
 
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
-        """
+    def __str__(self):
+        """a method that print class name, id"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """updats updated_at"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all
+        key and value  of an instance"""
+    my_dict = self.__dict__.copy()
+    for key, value in self.__dict__.items():
+        if key == 'created_at' or key == 'updated_at':
+            my_dict[key] = value.isoformat()
+
+    my_dict['__class__'] = type(self).__name__
+    return my_dict
+
+if __name__ == "__main__":
+
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module for BaseModel unittest
+"""
+import os
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBasemodel(unittest.TestCase):
+
+    """
+    Unittest for BaseModel
+    """
+    def test_save(self):
+        """
+        save method test
+        """
+        instanc = BaseModel()
+
+        ins_updated_at = instanc.updated_at
+
+        current_updated_at = instanc.save()
+
+        self.assertNotEqual(ins_updated_at, current_updated_at)
+
+    def test_to_dict(self):
+        """
+        Test for to_dict method
+        """
+        instan = BaseModel()
+
+        ins_dict = instan.to_dict()
+
+        self.assertIsInstance(ins_dict, dict)
+
+        self.assertEqual(ins_dict["__class__"], 'BaseModel')
+        self.assertEqual(ins_dict['id'], instan.id)
+        self.assertEqual(ins_dict['created_at'], instan.created_at.isoformat())
+        self.assertEqual(ins_dict["updated_at"], instan.created_at.isoformat())
+
+    def test_str(self):
+        """
+        string representation test
+        """
+        instanc = BaseModel()
+
+        self.assertTrue(str(instanc).startswith('[BaseModel]'))
+
+        self.assertIn(str(instanc.__dict__), str(instanc))
+
+        self.assertIn(instanc.id, str(instanc))
+
+
+if __name__ == "__main__":
+    unittest.main()
